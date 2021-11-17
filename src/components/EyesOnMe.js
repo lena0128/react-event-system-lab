c424811049734cb3072580d55d3f2bfa33a1076e
Lena Hou <lenahou2013@gmail.com>
2021-11-16 21:20:26 -0500

    commit tree 50d4eb6d041adc3bd9239bcf214624650733db58

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,21 @@
 // Code EyesOnMe Component Here
+
+import React from 'react'
+
+class EyesOnMe extends React.Component {
+    render(){
+        return(
+            <button onFocus={this.handleOnFocus} onBlur={this.handleOnBlur}>Click Me</button>
+        )
+    }
+
+    handleOnFocus = () => {
+        console.log("Good!")
+    }
+
+    handleOnBlur = () => {
+        console.log('Hey! Eyes on me!')
+    }
+}
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,19 @@
 // Code Keypad Component Here
+import React from 'react'
+
+class Keypad extends React.Component {
+    render(){
+        return(
+            <div>
+            <input type="password" onKeyUp={this.handleKeyUp} />
+            </div>
+        )
+    }
+
+    handleKeyUp = () => {
+        console.log("Entering password...")
+    }
+}
+
+
+export default Keypad
